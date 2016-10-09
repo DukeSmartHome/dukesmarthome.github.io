cbf8e12250bf22b65f9d6a14cf50a6e5d04b434b
Rvshi <harveyyshi@gmail.com>
2016-10-09 13:48:08 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+    // open and close nav menu
+    $('#close_container').click(function() {
+        $('nav').toggleClass('showNav');
+        $('#close').toggleClass('open');
+    });
+});
+
