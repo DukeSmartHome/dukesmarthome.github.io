1880cfa8551fe3f8b559e403cbbbc3fc9f729620
Joanna <four.easter@gmail.com>
2020-07-10 13:30:46 -0400

    Create 2021.js

---
+++ js/people/2021.js
@@ -0,0 +1,78 @@
+/*js/people/2021.js*/
+
+/* Data for people */
+var execs = [{
+    name: 'Rebecca Schmitt',
+    title: 'President',
+    picture: 'rebecca.JPG'
+}, {
+    name: 'Peter Candelora',
+    title: 'Vice-President',
+    picture: 'peter.JPG'
+}, {
+    name: 'David Smoot',
+    title: 'Treasurer',
+    picture: 'david.JPG'
+}, {
+    name: 'Himani Bhat',
+    title: 'Lab Manager, Communications Director',
+    picture: 'himani.JPG'
+}, {
+    name: 'Emma Rose Shore',
+    title: 'Garden Manager',
+    picture: 'emma.JPG'
+}, {
+    name: 'Gabe Goldhagen',
+    title: 'House Manager',
+    picture: 'gabe.JPG'
+}, {
+    name: 'Joanna Feaster',
+    title: 'Enrichment Director',
+    picture: 'joanna.JPG'
+}, {
+    name: 'Lucas Dingman',
+    title: 'General Exec Member',
+    picture: 'lucas.JPG'
+}
+];
+var residents = [{
+    name: 'Daniel Winkelman',
+    bio: '',
+    picture: 'daniel.JPG'
+}, {
+    name: 'Lucas Dingman',
+    bio: '',
+    picture: 'lucas.JPG'
+}, {
+    name: 'Riccardo Romersi',
+    bio: '',
+    picture: 'ricky.JPG'
+}, {
+    name: 'Elizabeth Bock',
+    bio: '',
+    picture: 'eliz.JPG'
+}, {
+    name: 'Himani Bhat',
+    bio: '',
+    picture: 'himani.JPG'
+}, {
+    name: 'Jolan von Plutzner',
+    bio: '',
+    picture: 'jolan.JPG'
+}, {
+    name: 'Ashley Wang',
+    bio: '',
+    picture: 'ashley.JPG'
+}, {
+    name: 'Joey Rauch',
+    bio: '',
+    picture: 'joey.JPG'
+}, {
+    name: 'Emma Rose Shore',
+    bio: '',
+    picture: 'emma.JPG'
+}, {
+    name: 'Gabriel Goldhagen',
+    bio: '',
+    picture: 'gabe.JPG'
+}];
