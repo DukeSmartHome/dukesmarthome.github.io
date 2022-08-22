029887801b95b5462b58be41913f03e2e2caa022
kelsey <59981704+theclimbingnerd@users.noreply.github.com>
2022-08-22 10:27:44 -0400

    create 2223 js with current exec and residents

---
+++ js/people/2223.js
@@ -0,0 +1,67 @@
+/*js/people/2022.js*/
+
+/* Data for people */
+// missing: communications director, technical director, enrichment director, community outreach chair
+var execs = [{
+    name: 'Zachary Karmel',
+    title: 'President',
+    picture: 'zach'
+}, {
+    name: 'Himani Bhat',
+    title: 'Vice President',
+    picture: 'himani'
+}, {
+    name: 'Gloria Odenyo',
+    title: 'Treasurer',
+    picture: 'gloria'
+}, {
+    name: 'Amy Yoon',
+    title: 'Garden Manager',
+    picture: 'amy'
+}, {
+    name: 'Surya Cannon',
+    title: 'House Manager',
+    picture: ''
+}, {
+    name: 'Kelsey Goldwein',
+    title: 'Project Manager & Website Manager',
+    picture: 'Kelsey'
+},
+];
+var residents = [{
+    name: 'Aryan Mathur',
+    bio: '',
+    picture: ''
+}, {
+    name: 'Bryce Fitzpatrick',
+    bio: '',
+    picture: 'Bryce'
+}, {
+    name: 'Eric Ma',
+    bio: '',
+    picture: 'Eric'
+}, {
+    name: 'Jason Fitzpatrick',
+    bio: '',
+    picture: 'Jason'
+}, {
+    name: 'Jerone Samari',
+    bio: '',
+    picture: ''
+}, {
+    name: 'Kelsey Goldwein',
+    bio: '',
+    picture: 'Kelsey'
+}, {
+    name: 'Ming Shen',
+    bio: '',
+    picture: ''
+}, {
+    name: 'Sarah Kelso',
+    bio: '',
+    picture: 'Sarah'
+}, {
+    name: 'Surya Cannon',
+    bio: '',
+    picture: ''
+}];
